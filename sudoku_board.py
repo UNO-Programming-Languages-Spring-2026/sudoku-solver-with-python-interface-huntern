7e2fa01ba8a91b3a69b36e46bff76bc29a71d0ad
Hunter Neff <hneff@unomaha.edu>
2026-03-26 12:23:24 -0500

    Commit 1 for questions 1 through 4

--- sudoku_board.py
+++ sudoku_board.py
@@ -9,6 +9,19 @@ class Sudoku:
     def __str__(self) -> str:
         s = ""
         # YOUR CODE HERE
+        for row in range(1, 10):
+            for col in range(1, 10):
+                val = self.sudoku[(row, col)]
+                s += str(val)
+                if col < 9:
+                    if col % 3 == 0:
+                        s += "  "
+                    else:
+                        s += " "
+            s += "\n"
+            if row % 3 == 0 and row < 9:
+                s += "\n"
+        #print(s)
         return s
 
     @classmethod
@@ -21,4 +34,10 @@ class Sudoku:
     def from_model(cls, model: clingo.solving.Model) -> "Sudoku":
         sudoku = {}
         # YOUR CODE HERE
+        for atom in model.symbols(shown = True):
+            if atom.name == "sudoku" and len(atom.arguments) == 3:
+                row = atom.arguments[0].number
+                col = atom.arguments[1].number
+                val = atom.arguments[2].number
+                sudoku[(row, col)] = val
         return cls(sudoku)
